77cd62c34ba622db63c50c06e3a99f7950b909fa
davefajardo <davefajardo915@gmail.com>
2026-03-06 12:00:37 +0800

    add root App component with screen routing

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { game } from './lib/state/gameState.svelte';
+  import StartScreen from './lib/screens/StartScreen.svelte';
+  import BoardScreen from './lib/screens/BoardScreen.svelte';
+  import ResultsScreen from './lib/screens/ResultsScreen.svelte';
+</script>
+
+<main class="h-full w-full">
+  {#if game.screen === 'start'}
+    <StartScreen />
+  {:else if game.screen === 'board' || game.screen === 'question'}
+    <BoardScreen />
+  {:else if game.screen === 'results'}
+    <ResultsScreen />
+  {/if}
+</main>
