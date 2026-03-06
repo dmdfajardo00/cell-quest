b5fc21ffd6ff2923edcec23dd03dbc995e9c7b61
davefajardo <davefajardo915@gmail.com>
2026-03-06 12:00:35 +0800

    add main entry point with Capacitor screen orientation lock

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { mount } from 'svelte'
+import './app.css'
+import App from './App.svelte'
+import { Capacitor } from '@capacitor/core'
+import { ScreenOrientation } from '@capacitor/screen-orientation'
+
+const app = mount(App, {
+  target: document.getElementById('app')!,
+})
+
+// Lock to landscape on native platforms
+if (Capacitor.isNativePlatform()) {
+  ScreenOrientation.lock({ orientation: 'landscape' })
+}
+
+export default app
